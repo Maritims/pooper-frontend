4364631f020f4f5958a1a955323bd79b5086a9ba
Martin Severin Steffensen <maritim@gmail.com>
2022-02-27 12:38:51 +0100

    Added test to ensure that translations aren't equal except for a few exceptions. Updated translations which weren't actually translated already.

--- test/translations.test.ts
+++ test/translations.test.ts
@@ -4,14 +4,25 @@ import nb from '../src/translations/nb';
 
 describe('Compare language files', () => {
     const nbKeys = [...Object.keys(nb)]; // All language files should be based on the Norwegian language file.
+    const nbVals = [...Object.values(nb)];
 
     [en].forEach(translations => {
         const keys = [...Object.keys(translations)];
+        const vals = [...Object.values(translations)];
         
         test('', () => expect(keys.length).toBe(nbKeys.length));
 
-        nbKeys.forEach(nbKey => {
+        nbKeys.forEach((nbKey, i) => {
             test(`Translation for '${nbKey}' should be present`, () => expect(keys.find(key => key === nbKey)).not.toBe(undefined));
+            
+            if([
+                'total',
+                'dropdown.filter.default.option'
+            ].indexOf(nbKey) !== -1) return;
+
+            const nbVal = nbVals[i];
+            const val = vals[keys.indexOf(nbKey)];
+            test(`Translations for '${nbKey}' should not be equal`, () => expect(nbVal).not.toBe(val));
         });
 
         keys.forEach(key => {
