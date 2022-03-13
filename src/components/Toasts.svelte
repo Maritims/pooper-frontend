e44387086768fa2fa230ab9bc0642c820db37232
Martin Severin Steffensen <maritim@gmail.com>
2022-03-13 21:53:20 +0100

    Added transition to toasts.

--- src/components/Toasts.svelte
+++ src/components/Toasts.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+    import { fade } from 'svelte/transition';
     import { toastStore, removeToast } from '../services/toasts';
 </script>
 
 <div class="toast-container">
     {#each $toastStore as toast}
-        <div class="toast show align-items-center text-white bg-{toast.type} border-0" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast show align-items-center text-white bg-{toast.type} border-0" role="alert" aria-live="assertive" aria-atomic="true" transition:fade>
             <div class="d-flex">
                 <div class="toast-body">{toast.body}</div>
                 <button type="button" class="btn-close btn-close-white me-2 m-auto" aria-label="Close" on:click={() => removeToast(toast)}></button>
